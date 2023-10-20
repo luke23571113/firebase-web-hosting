211af4813f9bfdae83b59c327dd8138d1f43ff38
Luke Collins <luke23571113@gmail.com>
2023-10-20 14:32:40 -0700

    day 2

--- public/script.js
+++ public/script.js
@@ -16,3 +16,64 @@ function switchContent() {
     }
     isHomePage = !isHomePage;
 }
+
+class Main {
+    constructor() {
+        this.pageViewsKey = 'pageViewsCount';
+        this.initializeCounter();
+        this.displayCount();
+    }
+
+    initializeCounter() {
+        if (!localStorage.getItem(this.pageViewsKey)) {
+            localStorage.setItem(this.pageViewsKey, '0');
+        }
+    }
+
+    incrementCount() {
+        let currentCount = parseInt(localStorage.getItem(this.pageViewsKey));
+        currentCount++;
+        localStorage.setItem(this.pageViewsKey, currentCount.toString());
+    }
+
+    displayCount() {
+        this.incrementCount();
+        document.getElementById('count').innerHTML = 'You have visited this page ' + localStorage.getItem(this.pageViewsKey)  + ' times.';
+    }
+
+    // Custom Function 1: Reset the counter to 0
+    resetCounter() {
+        localStorage.setItem(this.pageViewsKey, '0');
+        this.displayCount();
+    }
+
+    // Custom Function 2: Log the current counter value
+    logCounter() {
+        console.log(`Current Counter Value: ${localStorage.getItem(this.pageViewsKey)}`);
+    }
+
+    flashScreen() {
+        console.log("hit");
+        document.body.style.backgroundColor = 'purple';
+        setTimeout(() => {
+            document.body.style.backgroundColor = '';
+        }, 100);
+    }
+}
+
+document.mainClass = new Main();
+
+// Event Binding 1: Click event to reset the counter
+document.getElementById('resetButton').addEventListener('click', function() {
+    document.mainClass.resetCounter();
+});
+
+// Event Binding 2: Mousemove event to log the current counter value
+document.addEventListener('mousemove', function() {
+    document.mainClass.logCounter();
+});
+
+// Example for flashScreen:
+document.getElementById('flashScreenButton').addEventListener('click', function() {
+    document.mainClass.flashScreen();
+});
